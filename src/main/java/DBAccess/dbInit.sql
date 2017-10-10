c7c9a49070361a32f054214094bfef321cc7991b
GertMadsen <cph-gm50@cphbusiness.dk>
2017-10-10 23:30:30 +0200

    dbInit changes

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -1,9 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS `lego` DEFAULT CHARACTER SET latin1 ;
 USE `lego` ;
 
-CREATE TABLE IF NOT EXISTS `lego`.`user` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `lego`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(90) NOT NULL,
+  `email` VARCHAR(90) NOT NULL UNIQUE,
   `password` VARCHAR(45) NOT NULL,
   `role` VARCHAR(20) NOT NULL DEFAULT 'customer',
   PRIMARY KEY (`id`),
@@ -12,28 +13,27 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS `lego`.`order` (
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `lego`.`orders` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT(11) NOT NULL,
   `length` INT NOT NULL,
   `width` INT NOT NULL,
   `height` INT NOT NULL,
-  `date` VARCHAR(45) NOT NULL,
+  `date` DATETIME NOT NULL,
   `shipped` TINYINT NOT NULL,
   PRIMARY KEY (`id`, `user_id`),
   INDEX `fk_order_user_idx` (`user_id` ASC),
   CONSTRAINT `fk_order_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `lego`.`user` (`id`)
+    REFERENCES `lego`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-LOCK TABLES `user` WRITE;
-INSERT INTO `user` VALUES 
+LOCK TABLES `users` WRITE;
+INSERT INTO `users` VALUES 
 (1,'jens@somewhere.com','jensen','customer'),
 (2,'ken@somewhere.com','kensen','customer'),
 (3,'robin@somewhere.com','batman','employee');
 UNLOCK TABLES;
-
-
